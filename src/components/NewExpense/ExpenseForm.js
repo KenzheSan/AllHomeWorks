c942a257905f05192aca935b1adad4cf74d398a1
KenzheSan <js3nursultan@gmial.com>
2021-10-17 14:42:20 +0600

    17.10.21

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import './ExpenseForm.css'
 
 
-const ExpenseForm = () =>{
+const ExpenseForm = props =>{
 
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState('');
@@ -12,7 +12,7 @@ const ExpenseForm = () =>{
         setTitle(e.target.value)
     }
     const amountChangeHandler = (e) => {
-        setAmount(e.target.value)
+        setAmount(e.target.value)   
     }
     const dateChangeHandler = (e) => {
         setDate(e.target.value)
@@ -21,11 +21,15 @@ const ExpenseForm = () =>{
     const submitHandler = (e) =>  {
         e.preventDefault();
         let data = {
-            enteredTitle: title,
-            enteredAmount: amount,
-            enteredDate: new Date(date),
+            title: title,
+            amount: amount,
+            date: new Date(date),
         }
-        console.log(data);
+        props.onSaveExpenseData(data)
+        
+        setTitle('')
+        setAmount('')
+        setDate('')
     }
 
     return <form onSubmit={submitHandler}>
@@ -36,6 +40,7 @@ const ExpenseForm = () =>{
                 id="title"
                 type="text"
                 onChange={titleChangeHandler}
+                value = {title}
             />
         </div>
         <div className="new-expense__control">
@@ -45,6 +50,7 @@ const ExpenseForm = () =>{
                 min='0.01'
                 step='0.01'
                 onChange={amountChangeHandler}
+                value = {amount}
             />
         </div>
         <div className="new-expense__control">
@@ -54,12 +60,14 @@ const ExpenseForm = () =>{
                 min="2021-01-01"
                 max="2022-12-21"
                 onChange={dateChangeHandler}
+                value = {date}
             />
         </div>
     </div>
     <div className="new-expense__actions">
         <button type="submit">Add Expense</button>
     </div>
+
 </form>
 }
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,9 +2,16 @@ import React from "react"
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const onSaveExpenseDataHundler = data =>{
+        let expenseData = {
+            ...data,
+            id:Math.random().toString()
+        }
+        props.onAddExpense(expenseData)
+    }
     return <div className='new-expense'>
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData = {onSaveExpenseDataHundler}/>
     </div>
 }
 
