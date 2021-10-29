258fb638cc9e9a6ea8a58a7356193aec2dd16bbd
KenzheSan <js3nursultan@gmial.com>
2021-10-29 19:42:32 +0600

    29.10

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
-import Expenses from './components/Expense/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
-import React,{useState} from 'react';
-const expenses = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: 'e2',
-    title: 'New TV',
-    amount: 799.49,
-    date: new Date(2021, 2, 12)
-  },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
-
+import React from "react";
 
 function App() {
-  const [expenseData,setExpenseData] = useState(expenses)
-  
-  const addExpenseHundler = (expense) =>{
-    setExpenseData([
-      ...expenseData,
-      expense
-    ])
-  }
 
   return (
     <div>
-      <NewExpense onAddExpense = {addExpenseHundler}/>
-      <Expenses expenses={expenseData} />
+
     </div>
   );
 }
