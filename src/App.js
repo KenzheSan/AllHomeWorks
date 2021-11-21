4cc8bad997f2acef146735063f8bd76568670228
KenzheSan <js3nursultan@gmial.com>
2021-11-21 17:37:15 +0600

    OrederFood-part-1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
+
 function App() {
-	return <div></div>
+	return (
+		<>
+			<Header />
+			<main>
+				<Meals />
+			</main>
+		</>
+	)
 }
 
 export default App
---
+++ src/components/Meals/MealItemForm.js
@@ -0,0 +1,16 @@
+import classes from './MealItemForm.module.css'
+import Input from '../UI/Input'
+const MealItemForm = () =>{
+    return <form className={classes.form}>
+        <Input label = 'Amount' input ={{
+            id: 'Amount',
+            type: 'number',
+            min: '1',
+            max: '5',
+            defaultValue:'1',
+        }}/>
+        <button>+ Add</button>
+    </form>
+}
+
+export default MealItemForm
